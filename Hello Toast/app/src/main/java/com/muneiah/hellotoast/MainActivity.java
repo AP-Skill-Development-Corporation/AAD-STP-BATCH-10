c465c71d5921b906645d4bca7a7a3a0cef02236f
Muneiahtellakula <muneiahtellakula@gmail.com>
2020-08-07 14:46:06 +0530

    Hello Toast app  Fianl Day -5 for batch 10

--- Hello Toast/app/src/main/java/com/muneiah/hellotoast/MainActivity.java
+++ Hello Toast/app/src/main/java/com/muneiah/hellotoast/MainActivity.java
@@ -1,8 +1,10 @@
 package com.muneiah.hellotoast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 TextView data;
 Button c_button;
+int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,10 +21,30 @@ Button c_button;
         data=findViewById(R.id.tv);
         c_button=findViewById(R.id.countBtn);
 
+        if (savedInstanceState!=null && savedInstanceState.containsKey("muni")){
+            count=savedInstanceState.getInt("muni");
+            data.setText(String.valueOf(count));
+        }
+
+        c_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                data.setText(String.valueOf(count));
+            }
+        });
+
+
     }
 
     public void showToastMessage(View view) {
         //Toast.makeText(this,"welcome to app development",Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Welcome to app development", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("muni",count);
     }
 }
